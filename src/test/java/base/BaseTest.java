c2273faeb8c44641ff69f8ffd33028a88ee00e99
MaryMoroz <morozova.mary@gmail.com>
2023-10-30 22:11:19 -0400

    updated BaseTest

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -17,7 +17,7 @@ public abstract class BaseTest {
     @BeforeClass
     protected void launchBrowser() {
         playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        browser = playwright.chromium().launch();
     }
 
     @BeforeMethod
